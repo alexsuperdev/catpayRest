5564bc3a7bf379370ab8b3e013c851116d018c6f
maddl <matthias@Matthiass-MacBook.local>
2018-10-27 08:25:53 +0200

    next

---
+++ src/main/java/domiu/api/FrontConnector.java
@@ -0,0 +1,14 @@
+package domiu.api;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FrontConnector {
+	
+	@PostMapping
+	public void createAntrag(@RequestBody Auftrag auftrag) {
+		
+	}
+	
+
+}

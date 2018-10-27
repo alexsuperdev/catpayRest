dd21582b23295775dc02f134d0f864551eac5c94
superstar <oleksejk@gmail.com>
2018-10-27 08:21:02 +0200

    Initial commit

--- src/main/java/domiu/service/KonfipayService.java
+++ src/main/java/domiu/service/KonfipayService.java
@@ -1,11 +1,22 @@
 package domiu.service;
 
 import de.catpay.domiu.konfipay.dto.CustomerCreditTransferInitiationV03;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class KonfipayService {
 
+    private String endpoint = "https://portal.konfipay.de/api";
+
     public void erstelleLastschrift(CustomerCreditTransferInitiationV03 customerCreditTransferInitiationV03) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl
+                = "http://localhost:8080/spring-rest/foos";
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(fooResourceUrl + "/1", String.class);
     }
 }

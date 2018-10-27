34bd10d186ffbfd9693f192ee0c4d084a00c3150
superstar <oleksejk@gmail.com>
2018-10-27 10:16:05 +0200

    Anbindung an Konfipay. Login + Überweisung

---
+++ src/main/java/domiu/dto/Client.java
@@ -0,0 +1,23 @@
+package domiu.dto;
+
+public class Client {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    private String name;
+    private String version;
+}
